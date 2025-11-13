3a4569a0643ad78e4ae8e147284e9cd5160deb78
pedrofelipemoreira <pfelope@gmail.com>
2025-11-12 21:13:18 -0300

    criação do models de user

---
+++ backend/models/User.js
@@ -0,0 +1,40 @@
+import mongoose from "mongoose";
+
+const {Schema} = mongoose;
+
+
+const userSchema = new Schema({
+
+        name: {
+            type: String,
+            required: true
+        },
+
+        email: {
+            type: String,
+            required: true
+        },
+
+        password: {
+            type: String,
+            required: true
+        },
+
+        image: {
+            type: String
+        },
+
+        phone: {
+            type: String,
+            required: true
+        },
+
+    },
+    
+    {timestamps: true}
+
+)
+
+const User = mongoose.model('User', userSchema);
+
+export {User, userSchema};
